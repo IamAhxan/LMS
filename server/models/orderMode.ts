c418a4d6c533514e94a9fbf51af405f8a959f4ba
IamAhxan <mohammadahsan7744@gmail.com>
2026-05-05 12:40:47 +0500

    Order Model & Notification Model

---
+++ server/models/orderMode.ts
@@ -0,0 +1,29 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+export interface IOrder extends Document {
+  courseId: string;
+  userId: string;
+  payment_info: object;
+}
+
+const orderSchema: Schema<IOrder> = new Schema(
+  {
+    courseId: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    payment_info: {
+        type: Object,
+        // required: true
+    },
+  },
+  { timestamps: true },
+);
+
+const OrderModel: Model<IOrder> = mongoose.model<IOrder>("Order", orderSchema);
+
+export default OrderModel;
