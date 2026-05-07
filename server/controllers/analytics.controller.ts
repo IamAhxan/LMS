22ed094a137408121ff0bcbad0afad3620fe5a95
IamAhxan <mohammadahsan7744@gmail.com>
2026-05-07 20:35:23 +0500

    get user analytics admin only final

--- server/controllers/analytics.controller.ts
+++ server/controllers/analytics.controller.ts
@@ -10,6 +10,10 @@ export const getUsersAnalytics  = CatchAsyncError(
     async(req: Request, res: Response, next: NextFunction)=>{
         try {
             const users = await generateLast12MonthsData(userModel)
+            res.status(200).json({
+                success: true,
+                users,
+            });
         } catch (error: any) {
             return next(new ErrorHandler(error.message, 500))
         }
