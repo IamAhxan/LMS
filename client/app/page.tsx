827faa4098583d790a3a80ff60c14e87d8f8225d
IamAhxan <mohammadahsan7744@gmail.com>
2026-05-12 15:34:19 +0500

    next js setup undergo

---
+++ client/app/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+import React, {FC, useState} from 'react';
+import Heading from './utils/Heading';
+
+// interface Props{
+
+// }
+
+const Page= () => {
+  return (
+    <div>
+      <Heading
+      title='ELearning'
+      description='ELearning is an online learning platform'
+      keywords="Programming, MERN, Redux, Machine Learning"
+      />
+    </div>
+  )
+};
+
+export default Page;
